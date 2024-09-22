337e4299e99487fbfad8d673d022e05696e09553
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-09-22 21:23:30 +0300

    1

--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
-import "./ShopItemFunc.css";
+import "./ShopItemClass.css";
 export class ShopItemClass extends Component {
   render() {
     const { item } = this.props;
